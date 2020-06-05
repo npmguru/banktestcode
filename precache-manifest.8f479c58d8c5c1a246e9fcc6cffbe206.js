1f532559b5f034aa459c8725f8ec4fcdf0f14446
Anandkar <contactanandkar@gmail.com>
2020-06-06 00:56:38 +0400

    Updates

---
+++ precache-manifest.8f479c58d8c5c1a246e9fcc6cffbe206.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a306969387436965e55416b3ec7753",
+    "url": "/banktestcode/index.html"
+  },
+  {
+    "revision": "78d8d88b9e5efca2e926",
+    "url": "/banktestcode/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e93dd9b49812332f8bb1",
+    "url": "/banktestcode/static/css/main.132bdc21.chunk.css"
+  },
+  {
+    "revision": "78d8d88b9e5efca2e926",
+    "url": "/banktestcode/static/js/2.3c5e5061.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/banktestcode/static/js/2.3c5e5061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93dd9b49812332f8bb1",
+    "url": "/banktestcode/static/js/main.2e82c150.chunk.js"
+  },
+  {
+    "revision": "00875597f65beaaae87b",
+    "url": "/banktestcode/static/js/runtime-main.7ec96501.js"
+  },
+  {
+    "revision": "b314f5765962bf841c38c30e64f6d205",
+    "url": "/banktestcode/static/media/loading.b314f576.svg"
+  },
+  {
+    "revision": "0869a9814fef4d494ca7dc5ebee17c47",
+    "url": "/banktestcode/static/media/logo.0869a981.png"
+  },
+  {
+    "revision": "12dbd523f9f453760240f5ed9cc44247",
+    "url": "/banktestcode/static/media/rakbank-logo.12dbd523.jpg"
+  }
+]);
