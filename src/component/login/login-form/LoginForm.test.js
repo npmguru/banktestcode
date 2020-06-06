28bb3c3645e7d8af4abb65f0fa599a8739cc725c
Anandkar <contactanandkar@gmail.com>
2020-06-06 23:58:47 +0400

    Test Case Upload

--- src/component/login/login-form/LoginForm.test.js
+++ src/component/login/login-form/LoginForm.test.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import LoginForm from './LoginForm';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { useDispatch, useShallowEqualSelector } from 'react-redux';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../../persistence/reducers';
+import { AUTH_USER } from '../../../core/constant/app'
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-configure({ adapter: new Adapter() });
+
+jest.mock("react-redux", () => ({
+  connect: () => jest.fn(),
+  useSelector: jest.fn(fn => fn()),
+  useDispatch: () => jest.fn()
+}));
 
 describe('<LoginForm /> with no props', () => {
 
-  jest.mock('react-redux');
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  const container = shallow(<LoginForm />);
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
 
   it('should match the snapshot', () => {
+    const container = mount(<LoginForm />);
     expect(container.html()).toMatchSnapshot();
+
   });
 
   
